7c88b9b0fd2e29bc6a7430a92da059ddf7551b57
Rootselection <48713457+Rootselection@users.noreply.github.com>
2021-02-19 18:52:51 +0200

    +++

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+
+    $('.header-slider').slick({
+        arrows: false,
+        dots: true,
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      });
+});
